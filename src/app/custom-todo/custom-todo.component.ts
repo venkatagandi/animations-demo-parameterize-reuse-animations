0266493a1d1f64e86073f02319adfe44c58d9605
venkatagandi <venkata.gandi@gmail.com>
2018-08-06 08:46:41 +0100

    made changes

---
+++ src/app/custom-todo/custom-todo.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  transition,
+  style,
+  animate,
+  keyframes,
+  useAnimation
+} from "@angular/animations";
+import { bounceOutLeftAnimation, fadeInAnimation } from "../app.animations";
+
+@Component({
+  selector: "custom-todo",
+  templateUrl: "./custom-todo.component.html",
+  styleUrls: ["./custom-todo.component.css"],
+  animations: [
+    trigger("todoAnimation", [
+      transition(":enter", [
+        useAnimation(fadeInAnimation, {
+          params: {
+            duration: "500ms"
+          }
+        })
+      ]),
+      transition(":leave", [
+        style({ backgroundColor: "red" }),
+        animate(1000),
+        useAnimation(bounceOutLeftAnimation)
+      ])
+    ])
+  ]
+})
+export class CustomTodoComponent {
+  items: any[] = [
+    "Wash the dishes",
+    "Call the accountant",
+    "Apply for a car insurance"
+  ];
+
+  addItem(input: HTMLInputElement) {
+    this.items.splice(0, 0, input.value);
+    input.value = "";
+  }
+
+  removeItem(item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
